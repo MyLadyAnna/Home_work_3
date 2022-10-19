41df941a09765405dc2c0c4761349e68827a0aca
Anna <ann.sxd@example.com>
2022-10-19 14:30:44 +0300

    1 задача

---
+++ 01_week/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число обозначающее день недели. 
+// Выяснить является номер дня недели выходным
+Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int week = int.Parse(Console.ReadLine() ?? "0");
+
+if(week == 6 || week == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+Console.WriteLine("Будний день");
+}
